c9332fa1388e3b4121ba725563b3a2e538da6d0e
Iris <iris.hubert@efrei.net>
2020-10-19 11:06:12 +0200

    pages server et client sans register ni login

---
+++ server/data/articles.js
@@ -0,0 +1,15 @@
+const articles = [
+
+
+    /* {
+    id: 10,
+    name: 'Récupérateur d\'eau de pluie',
+    description: "C'est le moment idéal de l'année pour économiser !",
+    image: 'https://www.cdiscount.com/pdt2/3/4/2/1/300x300/ose4891372830342/rw/recuperateur-eau-de-pluie-%C3%B8-80-mm.jpg',
+    price: 11
+  }*/
+]
+
+module.exports = articles
+
+///là on met tous les voyages, du type le truc en commentaires
